b9f4678b7913f97442fbc10570ad3e0d8719942e
Ritesh Seth <ritesh@example.com>
2026-05-23 17:33:48 +0530

    fix: handle non-git extension directory in install script

--- install.sh
+++ install.sh
@@ -6,8 +6,14 @@ EXT_DIR="$HOME/.local/share/gnome-shell/extensions/touchpad-speed-control@ritesh
 echo "Installing Touchpad Speed Control..."
 
 if [ -d "$EXT_DIR" ]; then
-    echo "Updating existing installation..."
-    (cd "$EXT_DIR" && git pull)
+    if [ -d "$EXT_DIR/.git" ]; then
+        echo "Updating existing installation..."
+        (cd "$EXT_DIR" && git pull)
+    else
+        echo "Replacing existing installation..."
+        rm -rf "$EXT_DIR"
+        git clone https://github.com/ritesh-777/touchpad-speed-control.git "$EXT_DIR"
+    fi
 else
     git clone https://github.com/ritesh-777/touchpad-speed-control.git "$EXT_DIR"
 fi
